ef50073d7db020e049aac0d536f10cd7057ca320
bhumip214 <bhumip214@gmail.com>
2019-03-01 08:27:09 -0800

    Register function to implement register Api done

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,41 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
+const tokenService = require("../auth/token-service.js");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  try {
+    const { username, password } = req.body;
+    if (username && password) {
+      const hash = await bcrypt.hashSync(password, 14);
+
+      const [id] = await db("users").insert({ username, password: hash });
+      const user = await db("users")
+        .where({ id })
+        .first();
+
+      const token = tokenService.generateToken(user);
+      res.status(201).json({ user, token });
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide username and password to register."
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "There was an error while registering."
+    });
+  }
 }
 
 function login(req, res) {
@@ -18,15 +44,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
