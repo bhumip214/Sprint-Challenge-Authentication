2966d14ff551c824fc0cddd8abbd3f1513442101
bhumip214 <bhumip214@gmail.com>
2019-03-01 08:21:02 -0800

    updated aithenticate.js and created token-service.js to generate and verify token

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -6,17 +6,13 @@ module.exports = {
 };
 
 // implementation details
-function authenticate(req, res, next) {
+async function authenticate(req, res, next) {
   const token = req.get("Authorization");
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
-
-      req.decoded = decoded;
-
-      next();
-    });
+    const decoded = await tokenService.verifyToken(token);
+    req.decoded = decoded;
+    next();
   } else {
     return res.status(401).json({
       error: "No token provided, must be set on the Authorization Header"
